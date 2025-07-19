6d26b34044db31b8381c62b5424ad36528b3ab03
Amirul Shafiq Harun <amirul96shafiq.harun@gmail.com>
2025-07-19 23:21:17 +0800

    Redirecting logic for logged in and guest user in home page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    // If user is logged in, redirect to admin dashboard
+    if (Auth::check()) {
+        return redirect('/admin');
+    }
+
+    //If user not yet logged in, redirect to login page
+    return redirect('/admin/login');
 });
