0d92af7dbdea1ae49e2806cf14eb240fe5f89201
Amirul Shafiq Harun <amirul96shafiq.harun@gmail.com>
2025-07-29 23:19:40 +0800

    Implement Filter Tab by days in Document and Important URL modules

--- app/Filament/Resources/DocumentResource/Pages/ListDocuments.php
+++ app/Filament/Resources/DocumentResource/Pages/ListDocuments.php
@@ -6,10 +6,46 @@ use App\Filament\Resources\DocumentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListDocuments extends ListRecords
 {
     protected static string $resource = DocumentResource::class;
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+
+            'today' => Tab::make('Today')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->whereBetween('created_at', [
+                        now()->startOfDay(),
+                        now()->endOfDay(),
+                    ]);
+                }),
+
+            'this_week' => Tab::make('This Week')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->whereBetween('created_at', [
+                        now()->startOfWeek(),
+                        now()->endOfWeek(),
+                    ]);
+                }),
+
+            'this_month' => Tab::make('This Month')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->whereMonth('created_at', now()->month)
+                        ->whereYear('created_at', now()->year);
+                }),
+
+            'this_year' => Tab::make('This Year')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->whereYear('created_at', now()->year);
+                }),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
--- app/Filament/Resources/ImportantUrlResource/Pages/ListImportantUrls.php
+++ app/Filament/Resources/ImportantUrlResource/Pages/ListImportantUrls.php
@@ -6,11 +6,47 @@ use App\Filament\Resources\ImportantUrlResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 
 class ListImportantUrls extends ListRecords
 {
     protected static string $resource = ImportantUrlResource::class;
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+
+            'today' => Tab::make('Today')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->whereBetween('created_at', [
+                        now()->startOfDay(),
+                        now()->endOfDay(),
+                    ]);
+                }),
+
+            'this_week' => Tab::make('This Week')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->whereBetween('created_at', [
+                        now()->startOfWeek(),
+                        now()->endOfWeek(),
+                    ]);
+                }),
+
+            'this_month' => Tab::make('This Month')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->whereMonth('created_at', now()->month)
+                        ->whereYear('created_at', now()->year);
+                }),
+
+            'this_year' => Tab::make('This Year')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->whereYear('created_at', now()->year);
+                }),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
