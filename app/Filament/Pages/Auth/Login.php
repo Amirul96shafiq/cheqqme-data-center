5d293946e8406ebed872c45286988aa277b8172d
Amirul Shafiq Harun <amirul96shafiq.harun@gmail.com>
2025-07-31 15:57:49 +0800

    Removed Incorrect Login files/folders structure (unused) + Restructure the content of current Login page

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -2,23 +2,65 @@
 
 namespace App\Filament\Pages\Auth;
 
-use Filament\Pages\Page;
+use Filament\Forms;
+use FIlament\Forms\Form;
 use Filament\Pages\Auth\Login as BaseLogin;
 use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\Redirect;
+use Filament\Forms\Components\Actions\Action;
 
 
 class Login extends BaseLogin
 {
-    //protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
-    //protected static string $view = 'filament.pages.auth.login';
-
     protected static bool $shouldRegisterNavigation = false;
 
+    // Remove Default Form Action (Form buttons)
+    protected function getFormActions(): array
+    {
+        return [];
+    }
+    
+    // Styling customise Login Form
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('email')
+                    ->label('Email Address')
+                    ->email()
+                    ->autocomplete('email')
+                    ->required()
+                    ->autofocus(),
+
+                Forms\Components\TextInput::make('password')
+                    ->label('Password')
+                    ->password()
+                    ->required()
+                    ->revealable()
+                    ->autocomplete('password'),
+
+                Forms\Components\Actions::make([
+                    Action::make('login')
+                        ->label('Login')
+                        ->submit('login')
+                        ->extraAttributes(['class' => 'w-full py-4']),
+
+                    Action::make('forgotPassword')
+                        ->label('Forgot Password?')
+                        ->url(route('password.request'))
+                        ->color('gray')
+                        ->link()
+                        ->extraAttributes([
+                            'class' => 'w-full text-center mt-2 text-sm',
+                        ]),
+                ])
+                    ->columnSpanFull()
+                    ->columns(1),
+            ]);
+    }
     public function mount(): void
     {
-        //dd('Custom Login Loaded');
+        //dd('Custom Login Loaded'); // For testing loading
         parent::mount();
 
         $status = session()->pull('status');
