9b7d8ddab892b5bc95b56d4a88aa88e3bf34f8d8
Amirul Shafiq Harun <amirul96shafiq.harun@gmail.com>
2025-08-12 16:18:34 +0800

    Updated favicon at Forgot and Reset Password page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <title>Reset Password - CheQQme Data Center</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="/images/favicon.png">
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://unpkg.com/@tabler/icons@latest/iconfont/tabler-icons.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
