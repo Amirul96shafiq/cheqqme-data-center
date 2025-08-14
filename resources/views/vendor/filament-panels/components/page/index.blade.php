7751f6f6e53414db3a5ef90993131cb4672b7b47
Amirul Shafiq Harun <amirul96shafiq.harun@gmail.com>
2025-08-14 23:31:34 +0800

    Fix undefined issues in Index

--- resources/views/vendor/filament-panels/components/page/index.blade.php
+++ resources/views/vendor/filament-panels/components/page/index.blade.php
@@ -18,7 +18,7 @@
         ])
     }}
 >
-    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_START, scopes: $this->getRenderHookScopes()) }}
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_START, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
 
     <section
         @class([
@@ -65,7 +65,7 @@
         >
             @if ($subNavigation)
                 <div class="contents md:hidden">
-                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_SELECT_BEFORE, scopes: $this->getRenderHookScopes()) }}
+                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_SELECT_BEFORE, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
                 </div>
 
                 <x-filament-panels::page.sub-navigation.select
@@ -73,27 +73,27 @@
                 />
 
                 <div class="contents md:hidden">
-                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_SELECT_AFTER, scopes: $this->getRenderHookScopes()) }}
+                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_SELECT_AFTER, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
                 </div>
 
                 @if ($subNavigationPosition === SubNavigationPosition::Start)
-                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_START_BEFORE, scopes: $this->getRenderHookScopes()) }}
+                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_START_BEFORE, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
 
                     <x-filament-panels::page.sub-navigation.sidebar
                         :navigation="$subNavigation"
                     />
 
-                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_START_AFTER, scopes: $this->getRenderHookScopes()) }}
+                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_START_AFTER, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
                 @endif
 
                 @if ($subNavigationPosition === SubNavigationPosition::Top)
-                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_TOP_BEFORE, scopes: $this->getRenderHookScopes()) }}
+                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_TOP_BEFORE, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
 
                     <x-filament-panels::page.sub-navigation.tabs
                         :navigation="$subNavigation"
                     />
 
-                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_TOP_AFTER, scopes: $this->getRenderHookScopes()) }}
+                    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_TOP_AFTER, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
                 @endif
             @endif
 
@@ -103,7 +103,7 @@
                     'min-h-[65vh] max-h-[65vh] h-auto',
                 ])
             >
-                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_HEADER_WIDGETS_BEFORE, scopes: $this->getRenderHookScopes()) }}
+                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_HEADER_WIDGETS_BEFORE, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
 
                 @if ($headerWidgets = $this->getVisibleHeaderWidgets())
                     <x-filament-widgets::widgets
@@ -114,11 +114,11 @@
                     />
                 @endif
 
-                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_HEADER_WIDGETS_AFTER, scopes: $this->getRenderHookScopes()) }}
+                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_HEADER_WIDGETS_AFTER, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
 
                 {{ $slot }}
 
-                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_FOOTER_WIDGETS_BEFORE, scopes: $this->getRenderHookScopes()) }}
+                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_FOOTER_WIDGETS_BEFORE, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
 
                 @if ($footerWidgets = $this->getVisibleFooterWidgets())
                     <x-filament-widgets::widgets
@@ -129,17 +129,17 @@
                     />
                 @endif
 
-                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_FOOTER_WIDGETS_AFTER, scopes: $this->getRenderHookScopes()) }}
+                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_FOOTER_WIDGETS_AFTER, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
             </div>
 
             @if ($subNavigation && $subNavigationPosition === SubNavigationPosition::End)
-                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_END_BEFORE, scopes: $this->getRenderHookScopes()) }}
+                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_END_BEFORE, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
 
                 <x-filament-panels::page.sub-navigation.sidebar
                     :navigation="$subNavigation"
                 />
 
-                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_END_AFTER, scopes: $this->getRenderHookScopes()) }}
+                {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_SUB_NAVIGATION_END_AFTER, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
             @endif
         </div>
 
@@ -148,21 +148,41 @@
         @endif
     </section>
 
-    @if (! ($this instanceof \Filament\Tables\Contracts\HasTable))
+        @if (! ($this instanceof \Filament\Tables\Contracts\HasTable))
         <x-filament-actions::modals />
-    @elseif ($this->isTableLoaded() && filled($this->defaultTableAction))
+    @elseif (
+        method_exists($this, 'isTableLoaded')
+        && $this->isTableLoaded()
+    )
+        @php
+            $componentProperties = get_object_vars($this);
+            $defaultTableAction = $componentProperties['defaultTableAction'] ?? null;
+            $defaultTableActionRecord = $componentProperties['defaultTableActionRecord'] ?? null;
+            $defaultTableActionArguments = $componentProperties['defaultTableActionArguments'] ?? null;
+            $hasDefaultTableAction = filled($defaultTableAction);
+        @endphp
+
+        @if ($hasDefaultTableAction)
+            <div
+                wire:init="mountTableAction(@js($defaultTableAction), @if ($defaultTableActionRecord) @js($defaultTableActionRecord) @else {{ 'null' }} @endif @if ($defaultTableActionArguments) , @js($defaultTableActionArguments) @endif)"
+            ></div>
+        @endif
+    @endif
+
+    @php
+        $componentProperties = isset($componentProperties) ? $componentProperties : get_object_vars($this);
+        $defaultAction = $componentProperties['defaultAction'] ?? null;
+        $defaultActionArguments = $componentProperties['defaultActionArguments'] ?? null;
+        $hasDefaultAction = filled($defaultAction);
+    @endphp
+
+    @if ($hasDefaultAction)
         <div
-            wire:init="mountTableAction(@js($this->defaultTableAction), @if (filled($this->defaultTableActionRecord)) @js($this->defaultTableActionRecord) @else {{ 'null' }} @endif @if (filled($this->defaultTableActionArguments)) , @js($this->defaultTableActionArguments) @endif)"
+            wire:init="mountAction(@js($defaultAction) @if ($defaultActionArguments) , @js($defaultActionArguments) @endif)"
         ></div>
     @endif
 
-    @if (filled($this->defaultAction))
-        <div
-            wire:init="mountAction(@js($this->defaultAction) @if (filled($this->defaultActionArguments)) , @js($this->defaultActionArguments) @endif)"
-        ></div>
-    @endif
-
-    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_END, scopes: $this->getRenderHookScopes()) }}
+    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::PAGE_END, scopes: method_exists($this, 'getRenderHookScopes') ? $this->getRenderHookScopes() : []) }}
 
     <x-filament-panels::unsaved-action-changes-alert />
 </div>
