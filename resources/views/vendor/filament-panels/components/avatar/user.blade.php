c8139bbdc69a1f6cc8db3bcfa91ff5ec78936ba4
Amirul Shafiq Harun <amirul96shafiq.harun@gmail.com>
2025-09-13 02:30:48 +0800

    Enhance user avatar component with improved styling and dimensions
    - Added circular shape and size attributes to the user avatar for better visual consistency.
    - Updated avatar class to include a border and transition effects, enhancing user identification and interaction experience.

--- resources/views/vendor/filament-panels/components/avatar/user.blade.php
+++ resources/views/vendor/filament-panels/components/avatar/user.blade.php
@@ -5,8 +5,10 @@
 <x-filament::avatar
     :src="filament()->getUserAvatarUrl($user)"
     :alt="__('filament-panels::layout.avatar.alt', ['name' => filament()->getUserName($user)])"
+    :circular="true"
+    size="h-9 w-9"
     :attributes="
         \Filament\Support\prepare_inherited_attributes($attributes)
-            ->class(['fi-user-avatar'])
+            ->class(['fi-user-avatar border-2 border-primary-500 dark:border-primary-600 hover:border-primary-300 hover:dark:border-primary-500 rounded transition-colors duration-75'])
     "
 />
