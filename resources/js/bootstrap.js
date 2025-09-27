9401f30b0faab3ec50a61d492c975181b1583eee
Amirul Shafiq Harun <amirul96shafiq.harun@gmail.com>
2025-09-28 01:47:34 +0800

    Update broadcasting configuration to use Reverb
    - Changed the default broadcasting driver from Pusher to Reverb in the broadcasting configuration.
    - Updated the .env.example file to include new Reverb configuration settings.
    - Modified the bootstrap.js file to initialize Echo with Reverb settings for real-time communication.
    - Added Reverb-specific environment variables for improved integration and flexibility.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,14 +8,11 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: "pusher",
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
-    wsHost: import.meta.env.VITE_PUSHER_HOST
-        ? import.meta.env.VITE_PUSHER_HOST
-        : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusherapp.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
+    broadcaster: "reverb",
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? "localhost",
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "http") === "https",
     enabledTransports: ["ws", "wss"],
 });
