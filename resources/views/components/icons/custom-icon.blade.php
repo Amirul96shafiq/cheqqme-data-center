fea343bbd4c4d02b7592f99305d1020344fed58b
Amirul Shafiq Harun <amirul96shafiq.harun@gmail.com>
2025-10-02 00:00:40 +0800

    Add custom sidebar panel icon to Filament sidebar footer. Replace inline SVGs with reusable icon component for improved maintainability and consistency in the UI.

--- resources/views/components/icons/custom-icon.blade.php
+++ resources/views/components/icons/custom-icon.blade.php
@@ -156,6 +156,10 @@
                               </circle>
                           </svg>'
         ],
+        'sidebar-panel' => [
+            'viewBox' => '0 0 24 24',
+            'content' => '<path fill-rule="evenodd" d="M6 5 a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2 V5H6Zm4 0v14h8a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1h-8ZM3 6a3 3 0 0 1 3-3 h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Z" clip-rule="evenodd"/>'
+        ],
         // 'user' => [
         //     'viewBox' => '0 0 24 24',
         //     'content' => '<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"/>'
--- resources/views/filament/sidebar-footer.blade.php
+++ resources/views/filament/sidebar-footer.blade.php
@@ -9,9 +9,7 @@
         x-bind:title="'{{ __('filament-panels::layout.actions.sidebar.collapse.label') }}'"
         x-show="$store.sidebar.isOpen"
     >
-        <svg class="fi-sidebar-collapse-button-icon" fill="currentColor" viewBox="0 0 24 24">
-            <path fill-rule="evenodd" d="M6 5 a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2 V5H6Zm4 0v14h8a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1h-8ZM3 6a3 3 0 0 1 3-3 h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Z" clip-rule="evenodd"/>
-        </svg>
+        <x-icons.custom-icon name="sidebar-panel" class="fi-sidebar-collapse-button-icon" />
     </button>
 
     <!-- Expand button (shown when sidebar is closed) -->
@@ -24,8 +22,6 @@
         x-bind:title="'{{ __('filament-panels::layout.actions.sidebar.expand.label') }}'"
         x-show="!$store.sidebar.isOpen"
     >
-        <svg class="fi-sidebar-expand-button-icon" fill="currentColor" viewBox="0 0 24 24">
-            <path fill-rule="evenodd" d="M6 5 a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2 V5H6Zm4 0v14h8a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1h-8ZM3 6a3 3 0 0 1 3-3 h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Z" clip-rule="evenodd"/>
-        </svg>
+        <x-icons.custom-icon name="sidebar-panel" class="fi-sidebar-expand-button-icon" />
     </button>
 </div>
